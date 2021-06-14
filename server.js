94260137ef0e7f9cf6871859353572855b749695
Sanabel8 <sanabeljamal97@gmail.com>
2021-06-14 15:44:52 +0300

    added file

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express') // require the express package
 const app = express() // initialize your express app instance
 
 const cors = require('cors');
-
-app.use(cors()) // after you initialize your express app instance
 require('dotenv').config();
 
-const port =process.env.port;
+app.use(cors()) // after you initialize your express app instance
+
+const PORT=process.env.PORT;
 
 
 // a server endpoint 
@@ -15,4 +15,37 @@ app.get('/', // our endpoint name
   res.send('Hello World') // our endpoint function response
 })
  
-app.listen(port) // kick start the express server to work
+app.listen(PORT) // kick start the express server to work
+
+
+
+
+// const express = require('express') // require the express package
+// const app = express() // initialize your express app instance
+// const data = require('./assets/data.json');
+// require('dotenv').config();
+
+// const PORT = process.env.PORT;
+// const cors = require('cors'); // enable the communication between the frontend and the backend
+
+// app.use(cors())
+// // a server endpoint 
+// app.get('/', // our endpoint name
+//     function (req, res) { // callback function of what we should do with our request
+//         res.send('Hello World') // our endpoint function response
+//     }
+// );
+
+// app.get('/about-me', (request, response) => {
+//     response.send('My name is tamim!');
+
+// });
+
+// app.get('/weather-data', (req, res) => {
+//     res.json(data)
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server started on ${PORT}`);
+// });
+// // kick start the express server to work
